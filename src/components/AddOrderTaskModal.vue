c12b05a06550318f3682acc4199326d8ab1a4c5b
Ravi Lodhi <ravi.lodhi@hotwaxsystems.com>
2026-06-09 17:14:51 +0530

    Used icon-only for the close icon on modal.

--- src/components/AddOrderTaskModal.vue
+++ src/components/AddOrderTaskModal.vue
@@ -3,7 +3,7 @@
     <ion-toolbar>
       <ion-buttons slot="start">
         <ion-button @click="dismiss()">
-          <ion-icon :icon="closeOutline" />
+          <ion-icon slot="icon-only" :icon="closeOutline" />
         </ion-button>
       </ion-buttons>
       <ion-title>{{ translate('Add Task') }}</ion-title>
